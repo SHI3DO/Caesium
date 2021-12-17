e83cbc74591a65760be3f2dc4abe8a1c32f8531f
Shio <shio7113@gmail.com>
2021-12-17 13:29:27 +0900

    init

---
+++ index.ts
@@ -0,0 +1,17 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('Caesium - 1.0.0 SHI3DO')
+})
+
+client.login(process.env.TOKEN)
