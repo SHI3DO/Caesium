ffb015f4db02f78aebe716774cb7fa65d2f7688a
Shio <shio7113@gmail.com>
2021-12-18 12:57:58 +0900

    cae

---
+++ commands/utilities/math/division.ts
@@ -0,0 +1,30 @@
+import { ICommand } from 'wokcommands';
+import 'dotenv/config';
+import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
+
+export default {
+   category: 'Utilities/Math',
+   description: 'division',
+
+   options: [
+      {
+         name: 'num1',
+         description: 'first one',
+         required: true,
+         type: ApplicationCommandOptionTypes.NUMBER,
+      },
+      {
+         name: 'num2',
+         description: 'second one',
+         required: true,
+         type: ApplicationCommandOptionTypes.NUMBER,
+      },
+   ],
+   slash: true,
+
+   callback: ({ interaction }) => {
+      const num1 = interaction.options.getNumber('num1') || 0;
+      const num2 = interaction.options.getNumber('num2') || 0;
+      interaction.reply(`${num1} / ${num2} = ${num1 / num2}`);
+   },
+} as ICommand;
