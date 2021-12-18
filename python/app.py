74dc925759901770a6577ba830777cc00b100f0d
Shio <shio7113@gmail.com>
2021-12-18 13:38:58 +0900

    wolf

--- python/app.py
+++ python/app.py
@@ -1,19 +1,21 @@
 from flask import Flask
-import wolframalpha
+import wolframalpha as wf
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('home')
+@app.route('/home')
 def home():
     return 'Caesium'
 
 
 @app.route('/wolframalpha/<query>/<key>')
 def wolframalpha(query, key):
+    print("s")
+    print(query, key)
     app_id = key
-    client = wolframalpha.Client(app_id)
+    client = wf.Client(app_id)
     try:
         res = client.query(query)
         answer = next(res.results).text
